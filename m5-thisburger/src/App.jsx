c95b0528f68275a33a880d0949ba943dc4861c4b
Lucas_Passos <lucas_passos@msn.com>
2022-09-02 18:16:16 -0300

    Criacao das rotas das paginas cardapio, cart, pedido e landing page no app.jsx

--- m5-thisburger/src/App.jsx
+++ m5-thisburger/src/App.jsx
@@ -1,4 +1,7 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Cart } from "./components/Cart/Cart";
+import { Header } from './components/Header/Header';
+import { Cardapio } from './pages/Cardapio/Cardapio'; 
 import { LandingPage } from './pages/landingPage/LandingPage'
 import Pedido from './pages/pedido/Pedido'
 
@@ -7,8 +10,21 @@ function App() {
 
   return (
     <div className="App">
-        <LandingPage />
-        <Pedido />
+          <BrowserRouter>
+      <div className="App">
+        <div className="header">
+          <Header/>
+          </div>
+      <div className="mainContainer">
+        <Routes>
+          <Route path='/' element={<LandingPage/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/pedido' element={<Pedido/>}/>
+          <Route path='/cardapio' element={<Cardapio/>}/>
+        </Routes>
+      </div>
+      </div>
+    </BrowserRouter>
     </div>
   )
 }
