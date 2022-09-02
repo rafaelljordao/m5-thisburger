f72376dca44e26fca4cf9e6ecef2ce242d309abe
Joao P G Begiato <jpbegiato@hotmail.com>
2022-09-02 19:31:48 -0300

    integração da API com o cardápio

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -2,51 +2,26 @@ import { useReducer } from "react";
 import { useContext } from "react";
 import { createContext } from "react"
 import { cartReducer } from "./Reducers";
+import { listCardapio } from "../services/Request";
 import ifsalada from '../assets/if-salada.png'
 import backbacon from '../assets/back-bacon.png'
 import elsetudo from '../assets/else-tudo.png'
 import xbug from '../assets/x-bug.png'
 import frontegg from '../assets/front-egg.png'
 
+const images = [xbug, backbacon, frontegg, ifsalada, elsetudo]
 
 const Cart = createContext();
+const cardapio = await listCardapio()
+cardapio.map((element,index) =>{
+    element.image = images[index]
+})
+
 
 const Context = ({children}) => {
 
     const data = {
-        products: [
-            {
-                id: '1',
-                name: 'If-Salada',
-                price: 15,
-                image: ifsalada
-            },
-            {
-                id: '2',
-                name: 'Back-Bacon',
-                price: 25,
-                image: backbacon
-            },
-            {
-                id: '3',
-                name: 'Else-Tudo',
-                price: 31,
-                image: elsetudo
-            },
-            {
-                id: '4',
-                name: 'X-Bug',
-                price: 27,
-                image: xbug
-            },
-            {
-                id: '5',
-                name: 'Front-Egg',
-                price: 22,
-                image: frontegg
-            }
-           
-        ]
+        products: cardapio
     }
 
 
@@ -56,9 +31,9 @@ const Context = ({children}) => {
         cart: [],
     })
 
-  return (
+    return (
     <Cart.Provider value={{state, dispatch}}>{children}</Cart.Provider>
-  )
+    )
 }
 
 export default Context
@@ -66,3 +41,4 @@ export default Context
 export const CartState= () => {
     return useContext(Cart)
 }
+
