b697f15f66a672a600bec16a56d223a6051ffbe8
Lucas_Passos <lucas_passos@msn.com>
2022-09-02 18:22:50 -0300

    Criacao da branch Lucas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Cart } from "./components/Cart/Cart";
+import { Header } from './components/Header/Header';
+import { Cardapio } from './pages/Cardapio/Cardapio'; 
 import { LandingPage } from './pages/landingPage/LandingPage'
 import Pedido from './pages/pedido/Pedido'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
-        <LandingPage />
-        <Pedido />
+          <BrowserRouter>
+      <div className="App">
+        <div className="header">
+          <Header/>
+          </div>
+      <div className="mainContainer">
+        <Routes>
+          <Route path='/' element={<LandingPage/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/pedido' element={<Pedido/>}/>
+          <Route path='/cardapio' element={<Cardapio/>}/>
+        </Routes>
+      </div>
+      </div>
+    </BrowserRouter>
     </div>
   )
 }
---
+++ src/Context/Context.jsx
@@ -0,0 +1,68 @@
+import { useReducer } from "react";
+import { useContext } from "react";
+import { createContext } from "react"
+import { cartReducer } from "./Reducers";
+import ifsalada from '../../assets/if-salada.png'
+import backbacon from '../../assets/back-bacon.png'
+import elsetudo from '../../assets/else-tudo.png'
+import xbug from '../../assets/x-bug.png'
+import frontegg from '../../assets/front-egg.png'
+
+
+const Cart = createContext();
+
+const Context = ({children}) => {
+
+    const data = {
+        products: [
+            {
+                id: '1',
+                name: 'If-Salada',
+                price: 15,
+                image: ifsalada
+            },
+            {
+                id: '2',
+                name: 'Back-Bacon',
+                price: 25,
+                image: backbacon
+            },
+            {
+                id: '3',
+                name: 'Else-Tudo',
+                price: 31,
+                image: elsetudo
+            },
+            {
+                id: '4',
+                name: 'X-Bug',
+                price: 27,
+                image: xbug
+            },
+            {
+                id: '5',
+                name: 'Front-Egg',
+                price: 22,
+                image: frontegg
+            }
+           
+        ]
+    }
+
+
+
+    const [state, dispatch] = useReducer(cartReducer, {
+        data,
+        cart: [],
+    })
+
+  return (
+    <Cart.Provider value={{state, dispatch}}>{children}</Cart.Provider>
+  )
+}
+
+export default Context
+
+export const CartState= () => {
+    return useContext(Cart)
+}
