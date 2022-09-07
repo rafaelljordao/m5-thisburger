bc30c431808a16afa7451661cbba195704148e60
Rafael Jordão <rafaelimajordao@gmail.com>
2022-09-07 11:30:31 -0300

    Modificações no CSS da LandingPage, da imagem da section 2, e no título do Maps

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
     <Header />
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/pedido" element={<Pedido />} />
         <Route path="/cardapio" element={<Cardapio />} />
